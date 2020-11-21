4ad1638e1bd316e6bf47b4c251b8279ee6981190
Alexander Buell <abuell@email.arizona.edu>
2020-11-20 18:55:53 -0700

    Update

--- src/Connect4View.java
+++ src/Connect4View.java
@@ -1,12 +1,15 @@
 import java.util.Observable;
 import java.util.Observer;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
@@ -14,6 +17,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -42,10 +46,18 @@ public class Connect4View extends Application implements Observer{
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
+		
 	}
 	
+	/**
+	 * Takes a BorderPane object as an argument and adds a Menu and
+	 * MenuItem of “New Game” to build a menu bar.
+	 * 
+	 * @param bp, which represents a borderPane object.
+	 *
+	 * @return menuBar, which represents the built MenuBar object.
+	 */
 	private MenuBar buildMenuBar(BorderPane bp) {
-		System.out.println("Hi");
 		Menu menu = new Menu("File");
 		MenuBar menuBar = new MenuBar(menu);
 		MenuItem newGame = new MenuItem("New Game");
@@ -63,34 +75,22 @@ public class Connect4View extends Application implements Observer{
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
 				 //Stage other = new Stage();
-				Dialog<String> dialog = new Dialog<String>();
-				dialog.setTitle("Network Setup");
-				DialogPane dPane = new DialogPane() {
-	                @Override
-	                public Node createButtonBar() {
-	                    HBox box = new HBox(5);
-	                    Label label = new Label("Create:");
-	                    RadioButton server = new RadioButton("Server");
-	                    RadioButton client = new RadioButton("Client");
-	                    
-	                    
-	                    box.getChildren().add(super.createButtonBar());
-	                    box.getChildren().addAll(label,server, client);
-	                    return box ;
-	                }
-	            };
-	             dialog.setDialogPane(dPane);
-				 dialog.initModality(Modality.APPLICATION_MODAL);
-				 dialog.show();
-				
+				gameDialog gd = new gameDialog();
+				gd.show();
 			}
-			
 		});
-		
 		return menuBar;
-		
 	}
 	
+	/**
+	 * Takes a Connect4Controller and a BorderPane object as arguments
+	 * and builds the main board, which contains the game board and
+	 * the menu bar.
+	 * 
+	 * @param c, which represents the controller.
+	 * 
+	 * @param bp, which represents a borderPane object.
+	 */
 	private void buildBoard(Connect4Controller c, BorderPane bp) {
 		// TODO Auto-generated method stub
 		MenuBar menuBar = buildMenuBar(bp);
@@ -116,15 +116,67 @@ public class Connect4View extends Application implements Observer{
 		
 		
 		gPane.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-			int colNum = (int) event.getX();
-			System.out.println(colNum);
+			int n = (int) event.getX();
+			checkPos(c,bp,n);
 		});
-		
-		
-		
+			
+	}
 	
+	/**
+	 * Takes a Connect4Controller, a BorderPane, and an integer as arguments
+	 * to determine which column was clicked to add a color token.
+	 * 
+	 * @param c, which represents the controller.
+	 * 
+	 * @param bp, which represents a borderPane object.
+	 * 
+	 * @param n, which represents the X coordinate of the board.
+	 */
+	private void checkPos(Connect4Controller c, BorderPane bp, int n) {
+		int column = 6;
+		if(n <= 52) {
+			column = 0;
+			
+		}else if (n <=100) {
+			column = 1;
+			
+		}else if (n <= 148) {
+			column = 2;
+			
+		}else if (n <= 196) {
+			column = 3;
+			
+		}else if (n <= 244) {
+			column = 4;
+			
+		}else if (n <= 292) {
+			column = 5;
+		}
 		
+		c.humanTurn(column);
+		//turn(bp, c);
 		
+		c.computerTurn();
+		//turn(bp, c);
+			
+	}
+	
+	
+	
+	/**
+	 * Takes a Connect4Controller as an argument and checks
+	 * if the game has been won to display an alert.
+	 * 
+	 * @param c, which represents the controller.
+	 */
+	private void checkIfOver(Connect4Controller c) {
+		if(c.isGameOver()) {
+			Alert a = new Alert(Alert.AlertType.INFORMATION);
+			a.setTitle("Message");
+			a.setContentText("You won!");
+			a.setHeaderText("Message");
+			a.showAndWait();	
+		}	
 	}
 
 	/**
@@ -149,11 +201,100 @@ public class Connect4View extends Application implements Observer{
 		Scene scene = new Scene(pane, 344, 324);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Connect 4");
-		
-	    //primaryStage.initOwner(primaryStage);
-		
 		primaryStage.show();
 		
+	}
+	
+	private class gameDialog extends Stage {
+		public gameDialog() {
+			//Initialize HBoxes for all needed elements for dialog
+			HBox createElements = new HBox();
+			HBox playElements = new HBox();
+			HBox serverPortElements = new HBox();
+			HBox dialogButtons = new HBox();
+			VBox allElements = new VBox();
+			
+			fillCreateElements(createElements);
+			fillPlayElements(playElements);
+			fillServerPortElements(serverPortElements);
+			fillDialogButtons(dialogButtons);
+			
+			allElements.getChildren().addAll(createElements,playElements,
+            		serverPortElements);
+            
+            BorderPane bp = new BorderPane();
+            bp.setBottom(dialogButtons);
+            bp.setCenter(allElements);
+            
+            initModality(Modality.APPLICATION_MODAL);
+            Scene scene = new Scene(bp, 450, 200);
+            setScene(scene);
+            setTitle("Network Setup");
+            		
+		}
+
+		private void fillDialogButtons(HBox dialogButtons) {
+			//OK and cancel buttons
+            Button ok = new Button("OK");
+            Button cancel = new Button("Cancel");
+            
+            ok.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent event) {
+					// TODO Auto-generated method stub
+					hide();		
+				}
+            	
+            });
+            
+            cancel.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent event) {
+					// TODO Auto-generated method stub
+					hide();		
+				}
+            	
+            });
+            
+            dialogButtons.getChildren().addAll(ok,cancel);
+            dialogButtons.setPadding(new Insets(10));
+            dialogButtons.setSpacing(10);
+			
+		}
+
+		private void fillServerPortElements(HBox serverPortElements) {
+			//Server/Port section of dialog
+            Label serverL = new Label("Server");
+            TextField serverT = new TextField("localhost");
+            Label port = new Label("Port");
+            TextField portT = new TextField("4000");
+            serverPortElements.getChildren().addAll(serverL, serverT, port, portT);
+            serverPortElements.setPadding(new Insets(10));
+            serverPortElements.setSpacing(10);
+			
+		}
+
+		private void fillPlayElements(HBox playElements) {
+			//Play section of dialog
+            Label play = new Label("Play as:    ");
+            RadioButton human = new RadioButton("Human   ");
+            RadioButton computer = new RadioButton("Computer");
+            playElements.getChildren().addAll(play, human, computer);
+            playElements.setPadding(new Insets(10));
+			
+		}
+
+		private void fillCreateElements(HBox createElements) {
+			//Create section of dialog
+            Label create = new Label("Create:    ");
+            RadioButton server = new RadioButton("Server   ");
+            RadioButton client= new RadioButton("Client");
+            createElements.getChildren().addAll(create,server, client);
+            createElements.setPadding(new Insets(10));
+			
+		}
 	}
 
 }
